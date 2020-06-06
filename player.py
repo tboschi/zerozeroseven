72ed9a4445805a8264956610f75f5f93ba0a0a03
Tommaso <boschi.tommaso@gmail.com>
2020-06-06 17:41:40 +0100

    added bot class to player

--- player.py
+++ player.py
@@ -1,37 +1,22 @@
 import random
 
+
 class Player:
     """player class which can take actions"""
 
     def __init__(self):
         self.is_loaded = False     #bool
         self.is_shield = 3         #int
-        self.is_fired  = None      #player
 
 
     #############
     #game actions
 
-    def last_action(self):
-        """list of possible actions
-        
-        ! means fire
-        # means shield
-        * means load
-        """
-        if self.is_fired is not None:
-            return '!', self.is_fired
-        elif self.is_shield:
-            return '#', None
-        else:
-            return '*', None
 
-
-    def fire(self, player):
-        if self.can_fire() and player is not self:  #can fire only if loaded
+    def fire(self):
+        if self.can_fire():            #can fire only if loaded
             self.is_loaded = False     #used bullet
             self.is_shield = 3         #restore defend
-            self.is_fired  = player    #shooting player
             return True
         else:
             return False
@@ -41,7 +26,6 @@ class Player:
         if self.can_shield():
             #self.is_loaded = True     #shield does not change load status
             self.is_shield -= 1        #decrease defend
-            self.is_fired  = None      #not shooting 
             return True
         else:
             return False
@@ -51,7 +35,6 @@ class Player:
         if self.can_load():
             self.is_loaded = True      #new bullet
             self.is_shield = 3         #restore defend
-            self.is_fired  = None      #not shooting 
             return True
         else:                           #never here...
             return False
@@ -61,32 +44,34 @@ class Player:
     def can_fire(self):
         return self.is_loaded
 
-
     def can_load(self):             #always true, but can change
         return True
-
 
     def can_shield(self):
         return self.is_shield   #can defend max 3 times in a row
 
 
-
-    def random(self, uid, others):
-        """random action but need list of other players"""
-        act = '*'
-        if self.can_fire():
-            act += '!'
+    def actions(self):
+        act = []
+        if self.can_load():
+            act.append(self.load)
         if self.can_shield():
-            act += '#'
+            act.append(self.shield)
+        if self.can_fire():
+            act.append(self.fire)
 
-        choice = random.choice(act)
-        if choice == '!':
-            oid, tgt = random.choice(list(others.items()))
-            self.fire(tgt)
-            return oid
-        elif choice == '#':
-            self.shield()
+        return act
+
+
+class Bot(Player):
+
+    def take_action(self, uid, others):
+        """random action but need list of other players"""
+        choice = random.choice(self.actions())
+
+        if choice == self.shield:
             return uid
+        elif choice == self.fire:
+            return random.choice(others)
         else:
-            self.load()
             return None
